96f8273442c51035912987bbd894c7703018c5b0
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-01-24 11:27:00 -0700

    created 1st question in javascript

---
+++ questionsjavascript.js
@@ -0,0 +1,6 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["1. strings" , "2. booleans" , "3. alerts" , "4. numbers"],
+        answer: "alert"
+    }
