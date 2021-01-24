11fcda84a62621b29b1ea548767cc9341c0155e2
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-01-24 12:22:35 -0700

    added 2nd question

--- questionsjavascript.js
+++ questionsjavascript.js
@@ -1,6 +1,32 @@
 var questions = [
     {
         title: "Commonly used data types DO NOT include:",
-        choices: ["1. strings" , "2. booleans" , "3. alerts" , "4. numbers"],
+        choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
         answer: "alert"
-    }
+    },
+
+    {
+        title: "The condition in an if/else statement is enclosed within _____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+
+    {
+        title: "This edible mushroom is prized for it's delicious flavor",
+        choices: ["Amanita bisporigera", "Galerina marginata", "Morchella escuelenta", "Gravidex hombrii"],
+        answer: "Morchella escuelenta"
+    },
+
+    {
+        title: "The scientific name for a mushroom's stem is:",
+        choices: ["Stipe", "Stripe", "Stem", "Stipple"],
+        answer: "Stipe"
+    },
+
+    {
+        title: "This mushroom is known as the Chanterelle",
+        choices: ["Helvella", "Cantharellus", "Craterellus", "Laetiporus"],
+        answer: "Cantharellus"
+    },
+
+];
