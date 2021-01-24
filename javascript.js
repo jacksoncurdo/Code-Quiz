a35d1447db6ab6458275716e49eaa37738415522
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-01-24 10:13:19 -0700

    added container to index html and added 1st row

---
+++ javascript.js
@@ -0,0 +1,16 @@
+var score = 0;
+var banner = document.querySelector(".banner");
+var question = document.querySelector(".question");
+var highscore = document.querySelector(".highscore")
+var buttonsrow = document.querySelector(".buttons");
+var timer = document.querySelector(".timer");
+var timerInterval;
+var correctsound = document.querySelector(".correct");
+var wrongsound = document.querySelector(".wrong");
+var cw = document.querySelector(".cw");
+var cwtime = 2;
+var seconds = 180;
+var a1;
+var a2;
+var a3;
+var a4;
